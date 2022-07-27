0fbc56cf21c7dbe231cc3d67f3b85cd8ef335379
fengquanguo <fengquanguo@baijiayun.com>
2022-07-27 14:17:19 +0800

    feat: chapter 3 Custom-Types

---
+++ 3Custom-Types/Structures.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+struct Unit;
+
+struct Pair(i32, f32);
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn main() {
+    let name = String::from("Peter");
+
+    let age = 27;
+    let peter = Person { name, age };
+
+    println!("{:?}", peter);
+
+    let point: Point = Point { x: 10.3, y: 0.4 };
+
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let bottom_right = Point { x: 5.2, ..point };
+
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    let Point {
+        x: left_edge,
+        y: top_edge,
+    } = point;
+
+    let _rectangle = Rectangle {
+        top_left: Point {
+            x: left_edge,
+            y: top_edge,
+        },
+        bottom_right: bottom_right,
+    };
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+    //TODO
+    // 1.Add a function rect_area which calculates the area of a Rectangle
+    //   (try using nested destructuring).
+    // 2.Add a function square which takes a Point and a f32 as arguments,
+    //   and returns a Rectangle with its top left corner on the point,
+    //   and a width and height corresponding to the f32.
+}
